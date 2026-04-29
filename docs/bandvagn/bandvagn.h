0b3211a2ef955cc27fcee70f48753d17c23d7d8c
Simon Johansson <simon.johansson@student.tabyenskilda.se>
2026-04-29 21:19:50 +0200

    grundläggande http för bandvagn

---
+++ docs/bandvagn/bandvagn.h
@@ -0,0 +1,13 @@
+
+
+struct url_data {
+    size_t size;
+    char* data;
+};
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, struct url_data *data);
+
+char* http_get(char* url);
+
+
+
--- docs/bandvagn/main.c
+++ docs/bandvagn/main.c
@@ -7,10 +7,59 @@ Bandvagn package manager for Foglang
         ex:
         vagn install package_name
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+
+#include "bandvagn.h"
+
+#include "http.c"
+
+#define PACKAGES_PATH "https://github.com/simonballerina/foglang/docs/bandvagn/packages.json"
 
 int main(int argc, char *argv[]) {
-    
-    
-    
+
+    int install = 0;
+    int remove = 0;
+    char* package_to_modify = NULL;
+
+    if (argc > 1) {
+        if (strcmp(argv[1], "install") == 0) {
+            install = 1;
+            if (argc < 3) {
+                printf("No package name provided for installation\n");
+                return 1;
+            }
+            package_to_modify = argv[2];
+            printf("Installing package: %s\n", argv[2]);
+        } else if (strcmp(argv[1], "remove") == 0) {
+            remove = 1;
+            if (argc < 3) {
+                printf("No package name provided for removal\n");
+                return 1;
+            }
+            package_to_modify = argv[2];
+            printf("Removing package: %s\n", argv[2]);
+        } else {
+            printf("Unknown command: %s\n", argv[1]);
+            return -1;
+        }
+    } else {
+        printf("No command provided\n");
+        // todo: help
+
+        return -1;
+    }
+
+
+    printf("Locating package '%s'...\n", package_to_modify);
+
+    // Find packages json in Foglang github
+    char* packages = http_get(PACKAGES_PATH);
+    printf("packages: %s\n", packages);
+
+
+    free(packages);
     return 0;
 }
