abc931c163d633de36aba44d10f4893d291906bc
Simon Johansson <simon.johansson@student.tabyenskilda.se>
2026-04-29 20:25:28 +0200

    initial commit

---
+++ docs/bandvagn/main.c
@@ -0,0 +1,16 @@
+/*
+Bandvagn package manager for Foglang
+
+    Syntax:
+        vagn <command> <option> 
+
+        ex:
+        vagn install package_name
+*/
+
+int main(int argc, char *argv[]) {
+    
+    
+    
+    return 0;
+}
