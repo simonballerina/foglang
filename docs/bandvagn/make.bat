c1082f2e62498e47ef2a3987290c09b37ba26c4a
Simon Johansson <simon.johansson@student.tabyenskilda.se>
2026-05-01 23:29:17 +0200

    bandvagn install.py

---
+++ docs/bandvagn/make.bat
@@ -0,0 +1,27 @@
+@echo off
+
+REM --- default target ---
+set TARGET=%1
+
+REM If no argument passed, use default install location
+if "%TARGET%"=="" (
+    set TARGET=%LOCALAPPDATA%\Programs\foglang2\vagn.exe
+)
+
+echo Building to %TARGET%
+
+REM Ensure output directory exists
+for %%I in ("%TARGET%") do set OUTDIR=%%~dpI
+if not exist "%OUTDIR%" mkdir "%OUTDIR%"
+
+REM Compile
+gcc -o "%TARGET%" main.c -lcurl
+
+if %ERRORLEVEL% neq 0 (
+    echo Build failed!
+    exit /b %ERRORLEVEL%
+)
+
+echo Build succeeded!
+
+exit /b 0
